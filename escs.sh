3699e2c852c7e1e9e2806464b2c8385bad74dea1
Iury O. G. Figueiredo <last.src@gmail.com>
2025-09-24 14:15:50 -0300

    Project creation.

---
+++ escs.sh
@@ -0,0 +1,10 @@
+# Create the project.
+
+cd ~/projects/vilacic-code
+django-admin startproject vilacic .
+
+
+# Create main app.
+cd ~/projects/vilacic-code/
+python manage.py startapp site_app
+ls
